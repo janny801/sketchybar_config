a6f22365c80fffe01370f4cdc51a6a1089a5d09c
Janred Salubayba <jred8069@gmail.com>
2026-02-18 12:58:35 -0600

    enhance icons look and feel for spaces

--- plugins/space.sh
+++ plugins/space.sh
@@ -5,8 +5,16 @@ if [ "$SELECTED" = "true" ]; then
     background.drawing=on \
     background.height=2 \
     background.y_offset=12 \
-    background.color=0xffffffff
+    background.corner_radius=1 \
+    background.color=0xffffffff \
+    icon.background.drawing=on \
+    icon.background.color=0x22ffffff \
+    icon.background.corner_radius=6 \
+    icon.background.height=18 \
+    icon.background.padding_left=6 \
+    icon.background.padding_right=6
 else
   sketchybar --set "$NAME" \
-    background.drawing=off
+    background.drawing=off \
+    icon.background.drawing=off
 fi
