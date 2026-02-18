846e1fbafa9cc805c77c4a00a5781cec26a14654
Janred Salubayba <jred8069@gmail.com>
2026-02-18 12:58:06 -0600

    update to lowercase format since prettier ;p

--- plugins/clock_custom.sh
+++ plugins/clock_custom.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-# Clock plugin (custom)
-# Formats date like "Feb 16" and time like "11:42 PM"
 
-DATE_FORMAT="${DATE_FORMAT:-%b %e}"      # "Feb 16"
-TIME_FORMAT="${TIME_FORMAT:-%I:%M %p}"   # "11:42 PM"
+# Default formats (used if env not passed)
+DATE_FORMAT="${DATE_FORMAT:-%A, %b %e}"
+TIME_FORMAT="${TIME_FORMAT:-%I:%M %p}"
 
-DATE_STR="$(LC_TIME=en_US.UTF-8 date +"$DATE_FORMAT" | sed 's/  / /g')"
-TIME_STR="$(LC_TIME=en_US.UTF-8 date +"$TIME_FORMAT")"
+DATE=$(LC_TIME=en_US.UTF-8 date +"$DATE_FORMAT" | sed 's/  / /g')
+TIME=$(LC_TIME=en_US.UTF-8 date +"$TIME_FORMAT")
 
-# If you want a separator dot, change "  " to "  •  "
-sketchybar --set "$NAME" label="${DATE_STR}  ${TIME_STR}"
+# force lowercase
+DATE=$(echo "$DATE" | tr '[:upper:]' '[:lower:]')
+TIME=$(echo "$TIME" | tr '[:upper:]' '[:lower:]')
+
+sketchybar --set "$NAME" label="$DATE  $TIME"
